e6da6beb286d2e0a26f5df746f6368fc80ebf7ab
Daniel Schnabel <daniel@solarwinds.cloud>
2018-12-17 23:58:33 -0800

    lambda: retrieve payload from DB and write to S3

--- aws/lambda/db.go
+++ aws/lambda/db.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+    "strconv"
+    
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-west-2"))
 
-func putItem(ioTEvent *IoTEvent) error {
+func dbPutPacket(ioTEvent *IoTEvent) error {
     input := &dynamodb.PutItemInput{
         TableName: aws.String("Webcam"),
         Item: map[string]*dynamodb.AttributeValue{
@@ -21,11 +24,11 @@ func putItem(ioTEvent *IoTEvent) error {
             "message": {
                 N: aws.String(ioTEvent.Message),
             },
-            "packet": {
-                N: aws.String(ioTEvent.Packet),
-            },
             "type": {
                 N: aws.String(ioTEvent.Type),
+            },
+            "packet": {
+                N: aws.String(ioTEvent.Packet),
             },
             "payload": {
                 S: aws.String(ioTEvent.Payload),
@@ -35,4 +38,42 @@ func putItem(ioTEvent *IoTEvent) error {
 
     _, err := db.PutItem(input)
     return err
+}
+
+func dbGetMessage(ioTEvent *IoTEvent) (map[int]string, error) {
+    packets, err := strconv.Atoi(ioTEvent.Packet)
+    if err != nil {
+        return nil, err
+    }
+    
+    attr := []map[string]*dynamodb.AttributeValue{}
+    for i := 0; i < packets; i++ {
+        attr = append(attr, map[string]*dynamodb.AttributeValue{"id": {N: aws.String("1" + ioTEvent.Client + ioTEvent.Message + strconv.Itoa(i))}})
+    }
+    
+    input := &dynamodb.BatchGetItemInput{
+        RequestItems: map[string]*dynamodb.KeysAndAttributes{
+            "Webcam": {
+                Keys: attr,
+                ProjectionExpression: aws.String("packet, payload"),
+            },
+        },
+    }
+    
+    result, err := db.BatchGetItem(input)
+    if err != nil {
+        return nil, err
+    }
+    
+    payload := make(map[int]string)
+    for _, element := range result.Responses["Webcam"] {
+        dbItem := new(DbItem)
+        err = dynamodbattribute.UnmarshalMap(element, dbItem)
+        if err != nil {
+            return nil, err
+        }
+        payload[dbItem.Packet] = dbItem.Payload
+    }
+    
+    return payload, nil
 }
--- aws/lambda/main.go
+++ aws/lambda/main.go
@@ -4,8 +4,15 @@ import (
     "context"
     "log"
     "os"
+    "time"
+    "bytes"
 
     "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws/request"
 )
 
 var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
@@ -13,16 +20,78 @@ var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
 type IoTEvent struct {
         Client   string `json:"client"`
         Message  string `json:"message"`
-        Packet   string `json:"packet"`
         Type     string `json:"type"`
+        Packet   string `json:"packet"`
         Payload  string `json:"payload"`
 }
 
+type DbItem struct {
+        Packet  int    `json:"packet"`
+        Payload string `json:"payload"`
+}
+
 func router(ctx context.Context, event IoTEvent) {
-    err := putItem(&event)
-    if err != nil {
-        errorLogger.Println(err.Error())
+    if event.Type == "2" {
+        data, err := assemblePhotoData(&event)
+        if err != nil {
+            errorLogger.Println("Problem with fetching data")
+            errorLogger.Println(err)
+        } else {
+            saveToS3("abc.txt", data)
+        }
+    } else {
+        err := dbPutPacket(&event)
+        if err != nil {
+            errorLogger.Println(err.Error())
+        }
     }
+}
+
+func assemblePhotoData(event *IoTEvent) ([]byte, error) {
+    payload, err := dbGetMessage(event)
+    if err != nil {
+        return nil, err
+    }
+    var data string
+    for i := 0; i < len(payload); i++ {
+        data += payload[i] + "\n===\n"
+    }
+    
+    return []byte(data), nil
+}
+
+func saveToS3(filename string, data []byte) error {
+    sess := session.Must(session.NewSession())
+    svc := s3.New(sess)
+    
+    ctx := context.Background()
+    var cancelFn func()
+    
+    timeout, _ := time.ParseDuration("1m")
+    if timeout > 0 {
+        ctx, cancelFn = context.WithTimeout(ctx, timeout)
+    }
+    defer cancelFn()
+    
+    _, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+        Bucket: aws.String("remote-eye"),
+        Key:    aws.String(filename),
+        Body:   bytes.NewReader(data),
+    })
+    
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
+            errorLogger.Println("upload canceled due to timeout")
+            errorLogger.Println(err)
+        } else {
+            errorLogger.Println("failed to upload object")
+            errorLogger.Println(err)
+        }
+        
+        return err
+    }
+    
+    return nil
 }
 
 func main() {
